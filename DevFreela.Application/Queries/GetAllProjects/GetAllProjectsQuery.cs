79e5ed19fb7ef5769f97459eaee36f31e5c652f3
Leonardo Veras <leoveras7@hotmail.com>
2022-05-12 22:39:48 -0300

    Add Queries

---
+++ DevFreela.Application/Queries/GetAllProjects/GetAllProjectsQuery.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Queries.GetAllProjects
+{
+    public class GetAllProjectsQuery : IRequest<List<ProjectViewModel>>
+    {
+        public GetAllProjectsQuery(string query)
+        {
+            Query = query;
+        }
+
+        public string Query { get; private set; }
+    }
+}
