f960e6f54bc84a16c0f20a3531ec219ad59cdd86
Leonardo Veras <leoveras7@hotmail.com>
2022-05-23 15:35:58 -0300

    Refatorando as Queries

---
+++ DevFreela.Infrastructure/Persistence/Repositories/ProjectRepositoy.cs
@@ -0,0 +1,30 @@
+﻿using DevFreela.Core.Entities;
+using DevFreela.Core.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Infrastructure.Persistence.Repositories
+{
+    public class ProjectRepositoy : IProjectRepository
+    {
+        private readonly DevFreelaDbContext _dbContext;
+        private readonly string  _connectionString;
+
+
+        public ProjectRepositoy(DevFreelaDbContext dbContext, IConfiguration configuration)
+        {
+            _dbContext = dbContext;
+            _connectionString = configuration.GetConnectionString("DevFreelaCs");
+        }
+
+        public async Task<List<Project>> GetAll()
+        {
+            return await _dbContext.Projects.ToListAsync();
+        }
+    }
+}
---
+++ DevFreela.Infrastructure/Persistence/Repositories/SkillRepository.cs
@@ -0,0 +1,31 @@
+﻿using DevFreela.Core.DTOs;
+using DevFreela.Core.Repositories;
+using Microsoft.Extensions.Configuration;
+
+
+namespace DevFreela.Infrastructure.Persistence.Repositories
+{
+    public class SkillRepository : ISkillRepository
+    {
+        private readonly string _connectionString;
+
+        public SkillRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DevFreelaCs");
+        }
+
+        public async Task<List<SkillDTO>> GetAll()
+        {
+            using (var sqlConnection = new SqlConnection(_connectionString))
+            {
+                sqlConnection.Open();
+
+                var script = "SELECT Id, Description FROM Skills";
+
+                var skills = await sqlConnection.QueryAsync<SkillDTO>(script);
+
+                return skills.ToList();
+            }
+        }
+    }
+}
