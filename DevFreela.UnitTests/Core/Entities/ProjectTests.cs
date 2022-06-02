b4793861a6c32bc71281f0274869bb2cf7690ddf
Leonardo Veras <leoveras7@hotmail.com>
2022-06-01 23:26:31 -0300

    Criando testes unitários

---
+++ DevFreela.UnitTests/Core/Entities/ProjectTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.UnitTests.Core.Entities
+{
+    public class ProjectTests
+    {
+        [Fact]
+        public void TestIfProjectStarWorks()
+        {
+
+        }
+    }
+}
