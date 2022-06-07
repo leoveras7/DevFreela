332c4fdab7f8eff8a93d064d8df026944918a0d5
Leonardo Veras <leoveras7@hotmail.com>
2022-06-07 10:58:17 -0300

    Foi adicionado Testes Unitários

--- DevFreela.Core/Enums/ProjectStatusEnum.cs
+++ DevFreela.Core/Enums/ProjectStatusEnum.cs
@@ -3,7 +3,7 @@
     public enum ProjectStatusEnum
     {
         Created = 0,
-        InProgess = 1,
+        InProgress = 1,
         Suspended = 2,
         Cancelled = 3,
         Finished = 4,
--- DevFreela.UnitTests/Usings.cs
+++ DevFreela.UnitTests/Usings.cs
@@ -1,1 +1,6 @@
 global using Xunit;
+global using DevFreela.Core.Entities;
+global using DevFreela.Core.Enums;
+global using Moq;
+global using DevFreela.Core.Repositories;
+global using DevFreela.Application.Commands.CreateProject;
